13a513c0984bb5bec00cde07d393e1e62ced790c
Florian Bischoff <mail@florian-bischoff.de>
2023-01-28 18:05:31 +0100

    docs: add item selection

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export { CursorMarker, CustomMarker, NowMarker } from "./markers";
 export { CustomHeader, DateHeader, SidebarHeader, TimelineHeaders } from "./headers";
 export { defaultTimeSteps, defaultHeaderFormats } from "./default-config";
 export { Timeline, TimelineContext } from "./timeline";
+export type { Id } from "./shared-model"
 export type { TimelineProps } from "./timeline";
---
+++ stories/events.stories.tsx
@@ -0,0 +1,36 @@
+import "../src/timeline.scss";
+import { makeDemoData } from "./make-demo-data";
+import { Id, Timeline } from "../src";
+import React from "react";
+import type { Meta, StoryObj } from "@storybook/react";
+
+const meta = {
+  title: "Timeline/Events",
+  component: Timeline,
+  argTypes: {},
+} satisfies Meta<typeof Timeline>;
+
+export default meta;
+export type Story = StoryObj<typeof Timeline>;
+
+export const Events: Story = {
+  render: () => {
+    const [selected, setSelected] = React.useState<Id[]>([]);
+
+    return (
+      <div style={{ height: "100vh" }}>
+        <Timeline
+          {...makeDemoData()}
+          onCanvasClick={() => alert("Canvas clicked")}
+          onCanvasContextMenu={() => alert("Canvas context menu")}
+          onCanvasDrop={() => alert("Canvas drop")}
+          onItemClick={(id) => alert(`Item with id ${id} clicked`)}
+          onItemDoubleClick={(id) => alert(`Item with id ${id} double clicked`)}
+          onItemSelect={(a: Id) => setSelected([a])}
+          onItemDeselect={() => setSelected([])}
+          selected={selected}
+        />
+      </div>
+    );
+  },
+};
